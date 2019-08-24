d21fac6e9ca9a8d799e81b101fc3524bbbcfe9af
Mark Thomas <maes.tomos@gmail.com>
2019-08-24 08:40:21 +0100

    Initial Versions

--- cabinets/display-cabinets/display-cabinets-with-shelves/display-cabinets-with-shelves.ino
+++ cabinets/display-cabinets/display-cabinets-with-shelves/display-cabinets-with-shelves.ino
@@ -12,75 +12,118 @@
 
 #include <Adafruit_NeoPixel.h>
 
+class CabConfigData{
+  public:
+  uint16_t _pin;
+  uint16_t _nPixels;
+  uint16_t _nShelves;
+  uint16_t* _shelfSizes;
+  
+  CabConfigData(uint16_t pin,uint16_t nShelves,uint16_t* shelfSizes) :
+                                        _pin(pin),
+                                        _nPixels(0),
+                                        _nShelves(nShelves),
+                                        _shelfSizes(shelfSizes){
+    _nPixels=0;
+    for(uint16_t i=0; i<_nShelves; ++i){
+      _nPixels += 2*_shelfSizes[i];                                    
+    }
+  }
+};
+
+CabConfigData* cabConfigData[2];
+
 class CabConfig{
   private :
-  //uint16_t _nPixels;
-  //uint16_t _pin;
+  uint16_t _nShelves;
+  uint16_t* _shelfSizes;
   Adafruit_NeoPixel _strip;
-
+  
   public :
-  CabConfig(uint16_t nPixels,uint16_t pin) : _strip(nPixels,pin,NEO_GRB+NEO_KHZ800){
+  CabConfig(CabConfigData* cabConfigData) :
+  //uint16_t nPixels,uint16_t pin, uint16_t nShelves, uint16_t[] shelfSizes) :
+                                        _nShelves(cabConfigData->_nShelves),
+                                        _shelfSizes(cabConfigData->_shelfSizes),
+                                        _strip(cabConfigData->_nPixels,cabConfigData->_pin,NEO_GRB+NEO_KHZ800){
   }
   Adafruit_NeoPixel strip(){
     return _strip;
   }
 };
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define LED_PIN    6
-
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 60
-
-// Declare our NeoPixel strip object:
-CabConfig cabs[2] = {*(new CabConfig(60,6)),*(new CabConfig(60,7))};
 
 class Controller{
   private:
   uint16_t _nCabs;
-  CabConfig* _cabs;
-  
-  public:
-  Controller(uint16_t nCabs, CabConfig* cabs):_nCabs(nCabs),_cabs(cabs){ 
-  }
-  uint16_t nCabs(){
-     return _nCabs;
-  }
-  CabConfig* cabs(){
-    return _cabs;
-  }
+  CabConfig** _cabs;
+  // Declare our NeoPixel strip object:
+  //CabConfig* cabs;//[2] = {*(new CabConfig(60,6)),*(new CabConfig(60,7))};
+  //uint16_t nCabs(){
+  //   return _nCabs;
+  //}
+  //CabConfig* cab(uint16_t cabNo){
+  //  return _cabs[cabNo];
+  //}
   void clear(){
     for(int16_t i=0;i<_nCabs;++i){
-      _cabs[i].strip().clear();
+      _cabs[i]->strip().clear();
     }
   }
   void show(){
     for(int16_t i=0;i<_nCabs;++i){
-      _cabs[i].strip().show();
+      _cabs[i]->strip().show();
+    }
+  }
+  /*void setAllPixels(int32_t r, int32_t g, int32_t b) {
+    for(uint16_t i=0; i<_nCabs; ++i){
+      Adafruit_NeoPixel strip = _cabs[i]->strip();  
+      for(int i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.Color(r,g,b));
+      }
+    }
+    show();
+  }*/
+  
+  public:
+  Controller(uint16_t nCabs, CabConfigData** cabConfigData) : _nCabs(nCabs),_cabs(new CabConfig*[nCabs]){
+    for(int i=0; i<_nCabs; ++i){
+      _cabs[i] = new CabConfig(cabConfigData[i]);
+    }
+  }
+  void illuminateNextShelf(){
+    for(int i=0;i<_nCabs;++i){
+      CabConfig* cab = _cabs[i];
+      cab
+    }
+  }
+  void init(){
+    for(uint16_t i=0; i<_nCabs; ++i){
+      Adafruit_NeoPixel strip = _cabs[i]->strip();  
+      strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+      strip.show();            // Turn OFF all pixels ASAP
     }
   }
 };
 
-Controller* cntr = new Controller(2,cabs);
- 
-//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-
+Controller* cntr;
 
 // setup() function -- runs once at startup --------------------------------
 
 void setup() {
-  
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(75); // Set BRIGHTNESS to about 1/5 (max = 255)
+  cabConfigData[0] = new CabConfigData(7,2,new uint16_t[2]{4,3});
+  //cabConfigData[1] = new CabConfigData(8,2,new uint16_t[2]{3,4});
+  cntr = new Controller(1,cabConfigData);
+  cntr->init();
 }
 
 
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() {
+  cntr->illuminateNextShelf();
+}
+
+/*
 // loop() function -- runs repeatedly as long as board is on ---------------
 
 void loop() {
@@ -106,8 +149,11 @@ void loop() {
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
 void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+  for(uint16_t i=0; i<cntr.nCabs(); ++i){
+    Adafruit_NeoPixel strip = cntr.cabs()[i].strip();  
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    }
     strip.show();                          //  Update strip to match
     delay(wait);                           //  Pause for a moment
   }
@@ -119,12 +165,15 @@ void colorWipe(uint32_t color, int wait) {
 void theaterChase(uint32_t color, int wait) {
   for(int a=0; a<10; a++) {  // Repeat 10 times...
     for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      cntr.clear();         //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      for(uint16_t i=0; i<cntr.nCabs(); ++i){
+        Adafruit_NeoPixel strip = cntr.cabs()[i].strip();  
+        for(int c=b; c<strip.numPixels(); c += 3) {
+          strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        }
       }
-      strip.show(); // Update strip with new contents
+      cntr.show(); // Update strip with new contents
       delay(wait);  // Pause for a moment
     }
   }
@@ -137,19 +186,22 @@ void rainbow(int wait) {
   // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
   // means we'll make 5*65536/256 = 1280 passes through this outer loop:
   for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+      for(uint16_t i=0; i<cntr.nCabs(); ++i){
+        Adafruit_NeoPixel strip = cntr.cabs()[i].strip();  
+        for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+          // Offset pixel hue by an amount to make one full revolution of the
+          // color wheel (range of 65536) along the length of the strip
+          // (strip.numPixels() steps):
+          int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+          // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+          // optionally add saturation and value (brightness) (each 0 to 255).
+          // Here we're using just the single-argument hue variant. The result
+          // is passed through strip.gamma32() to provide 'truer' colors
+          // before assigning to each pixel:
+          strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+        }
     }
-    strip.show(); // Update strip with new contents
+    cntr.show(); // Update strip with new contents
     delay(wait);  // Pause for a moment
   }
 }
@@ -162,8 +214,7 @@ void theaterChaseRainbow(int wait) {
       cntr.clear();         //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in increments of 3...
       for(uint16_t i=0; i<cntr.nCabs(); ++i){
-        CabConfig cab = cntr.cabs()[i];
-        Adafruit_NeoPixel strip = cab.strip();  
+        Adafruit_NeoPixel strip = cntr.cabs()[i].strip();  
         for(int c=b; c<strip().numPixels(); c += 3) {
         // hue of pixel 'c' is offset by an amount to make one full
         // revolution of the color wheel (range 65536) along the length
@@ -178,3 +229,4 @@ void theaterChaseRainbow(int wait) {
     firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
   }
 }
+*/
