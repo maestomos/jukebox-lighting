850d557d9dfc91aa3774625cff765facac7c7e2d
maestomos <53418080+maestomos@users.noreply.github.com>
2019-09-02 21:44:21 +0100

    Update digital_jukebox.ino

--- jukeboxes/digital_jukebox/digital_jukebox.ino
+++ jukeboxes/digital_jukebox/digital_jukebox.ino
@@ -13,7 +13,7 @@
 #include <Adafruit_NeoPixel.h>
 
 
-#define DEBUG 1
+//#define DEBUG 1
 #ifndef DEBUG
   #define ENTER(f)
   #define PRINT(v)
@@ -49,6 +49,267 @@
   #define LEAVE debugTracer.leave();
   #define RETURN(v1,v2) debugTracer.rleave(v1); return v2;
 #endif
+
+
+class JukeboxConfigData{
+  public:
+  uint16_t _pin;
+  uint16_t _nPixels;
+  uint16_t _width,_height;
+    
+  JukeboxConfigData(uint16_t pin,uint16_t width,uint16_t height) :
+                                        _pin(pin),
+                                        _nPixels(0),
+                                        _width(width),
+                                        _height(height){
+    ENTER("JukeboxConfigData")
+    _nPixels=2*(_width+_height);
+    LEAVE
+  }
+};
+
+class RGB{
+  public:
+  uint8_t _r,_g,_b;
+  public:
+  RGB(uint8_t r,uint8_t g,uint8_t b):_r(r),_g(g),_b(b){    
+  }
+  uint32_t color(Adafruit_NeoPixel* strip) const{return strip->Color(_r,_g,_b);}
+};
+
+//static const uint16_t NUM_SHELVES=6;
+static const uint16_t NUM_JUKEBOXES=1;
+static const uint16_t NUM_COLORS=11;
+static const RGB COLORS[NUM_COLORS]={
+                                RGB(255,0,0),
+                                RGB(0,255,0),
+                                RGB(0,0,255),
+                                RGB(0,255,255),
+                                RGB(255,255,0),
+                                RGB(255,0,255),
+                                RGB(200,150,190),
+                                RGB(40,170,240),
+                                RGB(90,140,230),
+                                RGB(50,220,190),
+                                RGB(150,90,160)};
+JukeboxConfigData* jukeboxConfigData[NUM_CABINETS];
+
+class JukeboxConfig{
+  private:
+  uint16_t _width,_height;
+  Adafruit_NeoPixel* _strip;
+  
+  public:
+  JukeboxConfig(CabConfigData* cabConfigData) :
+                                        _width(cabConfigData->_width),
+                                        _height(cabConfigData->_height),
+                                        _strip(new Adafruit_NeoPixel(cabConfigData->_nPixels,cabConfigData->_pin,NEO_GRB+NEO_KHZ800)){
+    ENTER("JukeboxConfig")
+    PRINT(String("pin=")+jukeboxbConfigData->_pin)
+    LEAVE
+  }
+  
+  Adafruit_NeoPixel* strip(){
+    ENTER("strip")
+    RETURN("_strip",_strip);
+  }
+  void init(){
+    _strip->begin();
+    _strip->show();    
+  }
+  void test(){
+    ENTER("test")
+    teststrand();
+    LEAVE
+  }
+  uint16_t width(){
+    return _width;
+  }
+  uint16_t height(){
+    return _height;
+  }
+  //void setPixels(uint32_t c,uint16_t n){
+  //    setPixels(c,toOffset(n),_shelfSizes[n],false,0);
+  //}
+  private:
+  uint16_t toOffset(uint16_t n){
+    uint16_t offset=0;
+    for(uint16_t i=0;i<n;++i) offset+=_shelfSizes[i];
+    return offset;
+  }
+  void setPixels(uint32_t c,uint16_t offset, uint16_t s, boolean show,uint32_t wait){
+      ENTER("setPixels")
+      PRINT2("show=",show)
+      for(int j=0;j<s;++j){
+        _strip->setPixelColor(offset+j,c);
+        _strip->setPixelColor(_strip->numPixels()-1-offset-j,c);
+      }
+      if(show)_strip->show();
+      delay(wait);
+      LEAVE
+  }
+  void testshelves(){
+    for(int i=0;i<NUM_COLORS;++i){
+      testshelves(COLORS[i].color(_strip));
+    }
+  }
+  void testshelves(uint32_t c){
+    int16_t offset=0;
+    for(int i=0;i<_nShelves;++i){
+      const int16_t s=_shelfSizes[i];
+      setPixels(c,offset,s,true,100);
+      setPixels(0,offset,s,true,0);
+      offset+=s;
+    }
+  }
+  void teststrand(){    
+    ENTER("testinsequence")
+    for(int i=0;i<NUM_COLORS;++i){
+      teststrand(COLORS[i].color(_strip));
+    }
+    LEAVE
+  }
+  void teststrand(uint32_t c){
+    ENTER("testinsequence")    
+    for(int j=0;j<1;++j){
+      for(int i=0;i<_strip->numPixels();++i){
+        PRINT2("i=",i);
+        _strip->setPixelColor(i,c);
+        _strip->show();
+        delay(6);
+        _strip->setPixelColor(i,_strip->Color(0,0,0));
+        _strip->show();
+      }
+    }
+    LEAVE
+  }
+};
+
+class Controller{
+  private:
+  uint16_t _nCabs;
+  CabConfig** _cabs;
+  uint16_t _nextShelf;
+  uint16_t _nextColor;
+
+  void clear(){
+    for(int16_t i=0;i<_nCabs;++i){
+      _cabs[i]->strip()->clear();
+    }
+  }
+  void show(){
+    for(int16_t i=0;i<_nCabs;++i){
+      _cabs[i]->strip()->show();
+    }
+  }
+  
+  public:
+  Controller(uint16_t nCabs, CabConfigData** cabConfigData) :
+                          _nCabs(nCabs),_cabs(new CabConfig*[nCabs]),_nextShelf(0),_nextColor(0){
+    ENTER("Controller")
+    for(int i=0; i<_nCabs; ++i){
+      _cabs[i] = new CabConfig(cabConfigData[i]);
+    }
+    LEAVE
+  }
+  private:
+  void setPixels(uint8_t r,uint8_t g,uint8_t b){
+      for(int i=0;i<_nCabs;++i){
+        _cabs[i]->setShelfPixels(_cabs[i]->strip()->Color(r,g,b),_nextShelf%_cabs[i]->nShelves());
+      }    
+  }
+  void illuminate(uint8_t r,uint8_t g,uint8_t b,uint16_t w){
+      setPixels(r,g,b);
+      show();
+      delay(w);    
+  }
+  void fadeIn(){
+    const RGB rgb=COLORS[_nextColor];
+    for(int i=30;i>=1;--i){
+      illuminate(rgb._r/i,rgb._g/i,rgb._b/i,50);
+    }
+  }
+  void fadeOut(){
+    const RGB rgb=COLORS[_nextColor];
+    for(int i=1;i<=30;++i){
+      illuminate(rgb._r/i,rgb._g/i,rgb._b/i,50);
+    }
+    illuminate(0,0,0,0);
+  }
+  void illuminateAllShelvesInSequence()
+  {
+    const RGB rgb=COLORS[_nextColor];
+    for(int i=0;i<NUM_SHELVES;++i){
+      illuminate(rgb._r,rgb._g,rgb._b,500);
+      illuminate(0,0,0,0);
+      _nextShelf++;
+    }
+    _nextShelf-=2;
+    for(int i=0;i<NUM_SHELVES-1;++i){
+      illuminate(rgb._r,rgb._g,rgb._b,500);
+      illuminate(0,0,0,0);
+      _nextShelf--;
+    }
+      _nextShelf++; // Ensures that _nexShelf%NUM_SHELVES==0
+  }
+  void illuminateNextShelf(){
+    ENTER("illuminateNextShelf")
+    PRINT2("_nextColor=",_nextColor);
+    PRINT2("_nextShelf=",_nextShelf);
+    fadeIn();
+    delay(12000);
+    fadeOut();
+    _nextShelf++;
+    if(_nextShelf%NUM_SHELVES==0){
+      _nextColor++;
+      if(_nextColor%NUM_COLORS==0){
+        _nextColor=0;
+      }
+    }
+    LEAVE
+  }
+  public:
+  void test(){
+    for(uint16_t i=0;i<_nCabs;++i){
+      _cabs[i]->test();
+    }
+  }
+  void init(){
+    for(uint16_t i=0; i<_nCabs; ++i){
+      _cabs[i]->init();
+    }
+  }
+  void nextAction(){
+    if(_nextShelf%NUM_SHELVES==0){
+      illuminateAllShelvesInSequence();
+    }
+    illuminateNextShelf();
+  }
+};
+
+Controller* cntr;
+
+// setup() function -- runs once at startup --------------------------------
+
+void setup() {
+  DEBUG_SETUP
+  ENTER("setup")
+  cabConfigData[0] = new CabConfigData(7,NUM_SHELVES,new uint16_t[NUM_SHELVES]{5,7,8,10,8,6});
+  cabConfigData[1] = new CabConfigData(8,NUM_SHELVES,new uint16_t[NUM_SHELVES]{7,8,7,8,8,6});
+  cntr = new Controller(NUM_CABINETS,cabConfigData);
+  cntr->init();
+  cntr->test();
+  LEAVE
+}
+
+
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() {
+  ENTER("loop");
+  cntr->nextAction();
+  LEAVE
+}
 
 
 // Which pin on the Arduino is connected to the NeoPixels?
